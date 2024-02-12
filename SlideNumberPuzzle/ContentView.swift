51338144ee87519a0ff329de20ffa7704627a6bb
6410615147 <6410615147@student.tu.ac.th>
2024-02-12 15:23:54 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SlideNumberPuzzle
+//
+//  Created by นางสาวสุภาพันธ์ หง่อสกุล on 12/2/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
